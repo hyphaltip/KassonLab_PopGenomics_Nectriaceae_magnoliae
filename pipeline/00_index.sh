98885cdaa5323de3a9d70826e01497353cf82486
Jason Stajich <jasonstajich.phd@gmail.com>
2024-01-19 16:57:49 -0800

    fix dumping

--- pipeline/00_index.sh
+++ pipeline/00_index.sh
@@ -7,24 +7,9 @@ fi
 mkdir -p $GENOMEFOLDER
 pushd $GENOMEFOLDER
 # THIS IS EXAMPLE CODE FOR HOW TO DOWNLOAD DIRECT FROM FUNGIDB
-RELEASE=39
-SPECIES=AfumigatusAf293
-URL=https://fungidb.org/common/downloads/release-${RELEASE}/$SPECIES
-PREF=FungiDB-${RELEASE}_${SPECIES}
-FASTAFILE=${PREF}_Genome.fasta
-DOMAINFILE=${PREF}_InterproDomains.txt
-GFF=${PREF}.gff
-## THIS IS FUNGIDB DOWNLOAD PART
-echo "working off $FASTAFILE - check if these don't match may need to update config/init script"
-
-if [ ! -f $DOMAINFILE ]; then
-	curl -O $URL/txt/$DOMAINFILE
-fi
-if [ ! -f $FASTAFILE ] ; then
-	curl -O $URL/fasta/data/$FASTAFILE
-fi
-if [ ! -f $GFF ]; then
-	curl -O $URL/gff/data/$GFF
+FASTAFILE=Neonectria_magnoliae_FERN_10531.scaffolds.fa
+if [ ! -f $FASTAFILE ]; then
+	pigz -dc $FASTAFILE.gz > $FASTAFILE
 fi
 
 if [[ ! -f $FASTAFILE.fai || $FASTAFILE -nt $FASTAFILE.fai ]]; then
